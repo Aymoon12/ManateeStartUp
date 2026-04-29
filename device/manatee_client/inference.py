8ae4ef40d311047321a473d55de1ee3594ae7018
ayman anba <ayman_anba@wgresorts.com>
2026-04-28 21:08:41 -0400

    config

--- device/manatee_client/inference.py
+++ device/manatee_client/inference.py
@@ -157,8 +157,8 @@ class EdgeDetector:
 
     MAX_CLIPS = 60
     CLIP_THRESHOLD = 0.5
-    MIN_POSITIVE_CLIPS = 2
-    HIGH_CONFIDENCE_THRESHOLD = 0.85
+    MIN_POSITIVE_CLIPS = 3
+    HIGH_CONFIDENCE_THRESHOLD = 0.94
 
     def __init__(
         self,
