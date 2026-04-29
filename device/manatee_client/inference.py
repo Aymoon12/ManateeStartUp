b3909acb0aea45d0b9dc561d31e328f1dda5dfc5
ayman anba <ayman_anba@wgresorts.com>
2026-04-29 09:44:08 -0400

    min values

--- device/manatee_client/inference.py
+++ device/manatee_client/inference.py
@@ -157,7 +157,7 @@ class EdgeDetector:
 
     MAX_CLIPS = 60
     CLIP_THRESHOLD = 0.5
-    MIN_POSITIVE_CLIPS = 4
+    MIN_POSITIVE_CLIPS = 9
     HIGH_CONFIDENCE_THRESHOLD = 1.01  # disables OR rule; only count rule fires
 
     def __init__(
